61ddc7d41270294f52cfc61d2386e70c8e5fb963
mcgmark <mcgmark14@icloud.com>
2023-05-29 23:53:16 -0400

    Implemented Live Poll Count Updates

--- controllers/socketFunctions.js
+++ controllers/socketFunctions.js
@@ -3,23 +3,40 @@ const Poll = require('../models/poll');
 module.exports = io => {
     io.on('connection', (socket) => {
 
-    // Initialize variables to store id of player socket client
-    const userId = socket.id;
+        // Add user socket to pollId socketIO room for live updates
+        const pollId = socket.handshake.query.pollId;
+        socket.join(pollId);
 
-    // Server player connected message 
-    console.log('Socket Connected: ' + userId); 
+        // Initialize variables to store id of player socket client
+        const userId = socket.id;
+
+        // Server player connected message 
+        console.log('Socket Connected: ' + userId); 
+
+        // Handle join
+        socket.on('join', room => {
+            socket.join(room);
+            console.log(userId + " joined " + room);
+            io.to(room).emit('connected-to-poll');
+        });
+
+        // Handle poll votes
         socket.on('pollVote', async (id,vote) => {
             console.log('user voted');
             try {
-                const updatedPoll = await Poll.updateOne(
-                  { _id: id },
-                  { $inc: { [vote]: 1 } }
+                const updatedPoll = await Poll.findOneAndUpdate(
+                    { _id: id },
+                    { $inc: { [vote]: 1 } },
+                    { new: true } // To return the updated document
                 );
+                const voteCount = updatedPoll[vote];     
+                io.to(id).emit('update-poll-results', vote, voteCount);
                 console.log('vote success');
-              } catch (err) {
+            } catch (err) {
                 console.log('vote error');
-              }
+            }
         });
-      });  
-    
+
+      });    
+
 };
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,27 +1,43 @@
-const socket = io(); 
-
-function comparePasswords() {
-    let pw1 = document.getElementById('password').value;
-    let pw2 = document.getElementById('confirm').value;
-    let pwMessage = document.getElementById('pwMessage');
-
-    if (pw1 != pw2) {
-        pwMessage.innerText = "Passwords do not match";
-        pwMessage.className = "text-danger";
-        return false;
-    } else {
-        pwMessage.innerText = "";
-        pwMessage.className = "";
-        return true;
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
+    const socket = io(); 
+
+    function comparePasswords() {
+        let pw1 = document.getElementById('password').value;
+        let pw2 = document.getElementById('confirm').value;
+        let pwMessage = document.getElementById('pwMessage');
+
+        if (pw1 != pw2) {
+            pwMessage.innerText = "Passwords do not match";
+            pwMessage.className = "text-danger";
+            return false;
+        } else {
+            pwMessage.innerText = "";
+            pwMessage.className = "";
+            return true;
+        }
+    }
+
+    // grab poll id
+    const pollId = document.getElementById("vote-btn").dataset.pollid;
+    console.log(pollId);
+
+    // Join socketIO room with pollId
+    socket.emit('join', pollId);
+
+    // Listen for connection success message
+    socket.on('connected-to-poll', room => {
+       console.log('Live Poll Updates Started');
+    });
+
+    // Handle update votes
+    socket.on('update-poll-results', (answer, answerCount) => {
+        // Update the corresponding <span> element with the new vote count
+        document.getElementById(answer).textContent = answerCount;
+    });
 
     document.getElementById("vote-btn").onclick = function(e) {
         e.preventDefault();
         const vote = document.querySelector('input[name="vote"]:checked').value;
-        const pollId = this.dataset.pollid;
         console.log(pollId + ' ' + vote);
         socket.emit('pollVote', pollId, vote);
     };
