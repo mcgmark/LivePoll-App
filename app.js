fe26aa8aaa72d121b3f9ae080857e6914da96c18
mcgmark <mcgmark14@icloud.com>
2023-06-02 21:09:28 -0400

    added trust proxy

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.enable('trust proxy');
+
 // Add folder
 app.use(express.static(path.join(__dirname, 'public/')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
